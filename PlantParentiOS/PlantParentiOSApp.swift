6365ecd4fdf51e4adeb301f67ed067c55f5608ac
Francisco Barrientos <ftbarrientos103@gmail.com>
2025-10-23 16:13:46 +0200

    Initial Commit

---
+++ PlantParentiOS/PlantParentiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantParentiOSApp.swift
+//  PlantParentiOS
+//
+//  Created by stud on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantParentiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantParentiOSTests/PlantParentiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantParentiOSTests.swift
+//  PlantParentiOSTests
+//
+//  Created by stud on 23/10/2025.
+//
+
+import Testing
+@testable import PlantParentiOS
+
+struct PlantParentiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
