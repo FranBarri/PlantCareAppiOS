4bad7ced46085328cc08bda9e8c37ccc8575aa05
Francisco Barrientos <ftbarrientos103@gmail.com>
2025-11-20 16:34:42 +0100

    Finished homeView for now and added placeholders for the rest of the views

---
+++ PlantParentiOS/PlantLibraryView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct PlantLibraryView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Plant Library Placeholder")
+                .navigationTitle("Plant Library")
+        }
+    }
+}
+
+#Preview {
+    PlantLibraryView()
+}
